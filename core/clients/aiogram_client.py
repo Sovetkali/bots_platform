145a6794ed14e53b61d4554617408f5c05eebaa5
Sovetkali B <sovetkali@gmail.com>
2026-01-30 19:15:35 +0500

    Create FasAPI app, add main README.md

--- core/clients/aiogram_client.py
+++ core/clients/aiogram_client.py
@@ -1,3 +1,5 @@
+import asyncio
+import contextlib
 from aiogram import Bot, Dispatcher
 from core.base.client import BotClient
 from core.models.bot_mode import BotMode
@@ -19,4 +21,17 @@ class AiogramBotClient(BotClient):
             await self.bot.set_webhook(self.config.webhook_url)
 
     async def stop(self) -> None:
-        await self.bot.session.close()
+        logger.info(f"Stopping bot '{self.name}'.")
+
+        if self.config.mode == BotMode.WEBHOOK:
+            await self.bot.delete_webhook(drop_pending_updates=False)
+        elif self.config.mode == BotMode.POLLING:
+            #TODO никогда не попадаем сюда. Нужно разобраться.
+            # Для прода вроде как не критично, т.к. боты будут работаь на вебхуках.
+            await self.dp.stop_polling()
+
+        if self.bot.session:
+            await self.bot.session.close()
+            logger.info(f"Bot '{self.name}' session closed.")
+
+        logger.info(f"Bot '{self.name}' stopped.")
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import asyncio
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 import bots
 from core.registry import register_bot
 from utils.config import config
 from utils.logger import logger
 
-async def main():
+async def startup():
+    logger.info("Initializing and starting bots...")
     await register_bot.init_all()
     await register_bot.start_all()
+    logger.info("Bots started.")
+
+async def shutdown():
+    logger.info("Stopping bots...")
+    await register_bot.stop_all()
+    logger.info("Bots stopped.")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await startup()
+
+    yield
+
+    await shutdown()
+
+app = FastAPI(
+    lifespan=lifespan
+)
+
+# run python main.py for development
+
+async def main():
+    await startup()
 
 if __name__ == "__main__":
     asyncio.run(main())
